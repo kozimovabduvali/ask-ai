0edf7892a747f19c5ca1d52a2bd3d6e785a8fc38
Abduvalidev <abduvalidev1@gmail.com>
2024-12-20 00:56:01 +0500

    Нам доверяют люди со всей России

--- public/js/script.js
+++ public/js/script.js
@@ -33,3 +33,45 @@ function resetTyping() {
 }
 
 typeIt();
+
+
+
+
+
+// accordion
+const accordions = document.querySelectorAll(".accordion__item");
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const accordionIcons = document.querySelectorAll(".accordion__icon");
+const accordionContents = document.querySelectorAll(".accordion__content");
+
+// Har bir header uchun click hodisasi
+accordionHeaders.forEach((header, index) => {
+  header.addEventListener("click", () => {
+    const isActive = accordionContents[index].classList.contains("open");
+
+    // Barcha accordionlarni yopish
+    accordionContents.forEach((content, i) => {
+      content.classList.remove("open");
+      accordionIcons[i].classList.remove("rotate-45");
+      accordionIcons[i].classList.add("rotate-0");
+    });
+
+    // Faqat tanlangan accordionni ochish
+    if (!isActive) {
+      accordionContents[index].classList.add("open");
+      accordionIcons[index].classList.add("rotate-45");
+      accordionIcons[index].classList.remove("rotate-0");
+    }
+  });
+});
+
+// Tashqariga bosilganda barcha accordionlarni yopish
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".accordion__item")) {
+    accordionContents.forEach((content, i) => {
+      content.classList.remove("open");
+      accordionIcons[i].classList.remove("rotate-45");
+      accordionIcons[i].classList.add("rotate-0");
+    });
+  }
+});
